44a49d60da791942603283791a33e76a7894672f
José GAAAD <jose@gmail.com>
2023-06-17 11:58:01 -0500

    creación de la estructura y model

---
+++ src/main/java/com/sb/appWeb/ProyectoSpeedyBitesApplication.java
@@ -0,0 +1,13 @@
+package com.sb.appWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoSpeedyBitesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoSpeedyBitesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sb/appWeb/ProyectoSpeedyBitesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sb.appWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoSpeedyBitesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
