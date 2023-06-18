08ee5092a2f5db6d2bddcd37e57487e28834ce1d
José GAAAD <jose@gmail.com>
2023-06-18 01:26:12 -0500

    implementacion para guardar imagen del producto

--- src/main/java/com/sb/appWeb/controller/ProductoController.java
+++ src/main/java/com/sb/appWeb/controller/ProductoController.java
@@ -1,5 +1,6 @@
 package com.sb.appWeb.controller;
 
+import java.io.IOException;
 import java.util.Optional;
 
 import org.slf4j.*;
@@ -10,10 +11,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.sb.appWeb.model.Producto;
 import com.sb.appWeb.model.Usuario;
 import com.sb.appWeb.service.ProductoService;
+import com.sb.appWeb.service.UploadFileService;
 
 
 @Controller
@@ -24,6 +28,9 @@ public class ProductoController {
 
 	@Autowired
 	private ProductoService productoService;
+	
+	@Autowired
+	private UploadFileService upload;
 	
 	@GetMapping("")
 	public String show(Model model) {
@@ -37,11 +44,23 @@ public class ProductoController {
 	}
 	
 	@PostMapping("/save")
-	public String save(Producto producto) {
+	public String save(Producto producto, @RequestParam("img") MultipartFile file) throws IOException {
 		LOGGER.info("Este es el objeto producto {}", producto);
 		
 		Usuario u = new Usuario(1, "","", "","","","","");
 		producto.setUsuario(u);
+		
+		//images
+		if (producto.getId()==null) { //cuando se crea un producto
+			String nombreImagen = upload.saveImage(file);
+			producto.setImagen(nombreImagen);
+		}else {
+			if(file.isEmpty()) {
+				Producto p = new Producto();
+				p = productoService.get(producto.getId()).get();
+				producto.setImagen(p.getImagen());
+			}
+		}
 		
 		productoService.save(producto);
 		return "redirect:/productos";
